2aa20388883db829f34bd08076dfe0ce6e93cb02
ilyayevtushenko <ilyayevtushenko@gmail.com>
2018-06-13 15:36:11 +0300

    [eq

---
+++ seashark1/ConsoleApp1/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int size;
+        static int[,] matrix;
+        static int[] enters;
+        static int[] exits;
+        static int[,] paths1;
+        static int[,] paths2;
+
+        static void Main(string[] args)
+        {
+            getMatrix();
+            calc1Answer();
+            calc2Answer();
+            Console.ReadLine();
+        }
+        static void getMatrix() {
+            getSize();
+            matrix = new int[size, size];
+            if (isAutoMatrix())
+            {
+                AutoMatrix();
+            }
+            else
+            {
+                UserMatrix();
+            }
+            CheckInputs();
+        }
+        static void AutoMatrix() {
+            Random random = new Random();
+                enters = new int[size/5 + 1];
+                exits = new int[size/5 + 1];
+            for (int i = 1; i < size; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    matrix[i, j] = matrix[j, i] = (random.NextDouble() < 0.65) ? 1 : 0;
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                matrix[i, i] = 0;
+            }
+            for(int i = 0; i < enters.Length; i++)
+            {
+                enters[i] = random.Next(size);
+                for (int j = 0; j < i; j++)
+                {
+                    if(enters[i] == enters[j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < exits.Length; i++)
+            {
+                exits[i] = random.Next(size);
+                for (int j = 0; j<i; j++)
+                {
+                    if (exits[i] == exits [j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+                for (int k = 0; k < enters.Length; k++)
+                {
+                    if (exits[i] == enters[k])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+        }
+        static void UserMatrix() {
+            for (int i = 0; i < size; i++)
+            {
+                string[] listLine = Console.ReadLine().Split(' ');
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(listLine.ElementAt(j));
+                }
+            }
+            Console.WriteLine("Введите все входы: ");
+            string[] entersString = Console.ReadLine().Split(' ');
+            enters = new int[entersString.Length];
+            for (int i = 0; i < entersString.Length; i++)
+            {
+                enters[i] = Convert.ToInt32(entersString[i]);
+            }
+            Console.WriteLine("Введите все выходы: ");
+            string[] exitsString = Console.ReadLine().Split(' ');
+            
+            exits = new int[exitsString.Length];
+            for (int i = 0; i < exitsString.Length; i++)
+            {
+                exits[i] = Convert.ToInt32(exitsString[i]);
+            }
+
+        }
+        static void CheckInputs()
+        {
+            Console.WriteLine("Матрица: ");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j<size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Входы: ");
+            for (int i = 0; i < enters.Length; i++)
+            {
+                Console.Write(enters[i] + " ");
+            }
+            Console.WriteLine("\nВыходы: ");
+            for (int i = 0; i < exits.Length; i++)
+            {
+                Console.Write(exits[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void getSize() {
+            Console.WriteLine("Введите размерность матрицы: ");
+            size = Convert.ToInt32(Console.ReadLine());
+        }
+        static bool isAutoMatrix()
+        {
+            Console.WriteLine("Сгенерировать матрицу автоматически?[y/n]");
+            char answer = Console.ReadLine().ElementAt(0);
+            if (answer.Equals('y')) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void calc1Answer() {
+            int answers = 0;
+            List<int> usedNodes = new List<int>();
+            Queue<int> currNodes = new Queue<int>();
+            List<int> entersList = enters.ToList();
+            while(entersList.Count > 0)
+            {
+                int currEnter = entersList.ElementAt(0);
+                entersList.Remove(currEnter);
+                currNodes.Enqueue(currEnter);
+                while(currNodes.Count > 0)
+                {
+                    int currNode = currNodes.Dequeue();
+                    usedNodes.Add(currNode);
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (matrix[currNode, i] == 1)
+                        {
+                            if (exits.Contains(i))
+                            {
+                                //Console.WriteLine("exit: " + i);
+                                answers++;
+                                if (enters.Contains(currNode))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            if (usedNodes.Contains(i))
+                            {
+                                //Console.WriteLine("used node: " + i);
+                                continue;
+                            }
+                            if (entersList.Contains(i))
+                            {
+                                //Console.WriteLine("enter: " + i);
+                                //currNodes.Enqueue(i);
+                                entersList.Remove(i);
+                            }
+                            //Console.WriteLine("add node: " + i);
+                            currNodes.Enqueue(i);
+                            //usedNodes.Add(i);
+                            if (enters.Contains(currNode))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }         
+            }
+            Console.WriteLine("Ответ1: " + answers);
+        }
+        static void calc2Answer() {
+            int answers = 0;
+            List<int> usedNodes = new List<int>();
+            Queue<int> currNodes = new Queue<int>();
+            List<int> entersList = enters.ToList();
+            while (entersList.Count > 0)
+            {
+                int currEnter = entersList.ElementAt(0);
+                entersList.Remove(currEnter);
+                currNodes.Enqueue(currEnter);
+                while (currNodes.Count > 0)
+                {
+                    int currNode = currNodes.Dequeue();
+                    usedNodes.Add(currNode);
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (matrix[currNode, i] == 1)
+                        {
+                            if (exits.Contains(i))
+                            {
+                                //Console.WriteLine("exit: " + i);
+                                answers++;
+                                if (enters.Contains(currNode))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                            if (usedNodes.Contains(i))
+                            {
+                                //Console.WriteLine("used node: " + i);
+                                continue;
+                            }
+                            if (entersList.Contains(i))
+                            {
+                                //Console.WriteLine("enter: " + i);
+                                //currNodes.Enqueue(i);
+                                entersList.Remove(i);
+                            }
+                            //Console.WriteLine("add node: " + i);
+                            currNodes.Enqueue(i);
+                            usedNodes.Add(i);
+                            if (enters.Contains(currNode))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Ответ2: " + answers);
+        }
+
+
+    }
+}
---
+++ seashark2/seashark2/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace seashark2
+{
+    class Program
+    {
+        public abstract class Currency
+        {
+            protected double Money { get; set; }
+            public abstract Currency ToDollar();
+            public abstract Currency ToEuro();
+            public abstract Currency ToPound();
+            public abstract Currency ToHryvnya();
+
+        }
+        public class Dollar : Currency
+        {
+            public double number
+            {
+                get
+                {
+                    return Money;
+                }
+                set
+                {
+                    Money = value;
+                }
+            }
+
+            public override Currency ToDollar()
+            {
+                Dollar newvalue = new Dollar();
+                newvalue.number = this.number;
+                return newvalue;
+            }
+
+            public override Currency ToEuro()
+            {
+                Dollar newvalue = new Dollar();
+                newvalue.number = this.number * 1.2;
+                return newvalue;
+            }
+            public override Currency ToHryvnya()
+            {
+                Dollar newvalue = new Dollar();
+                newvalue.number = this.number * 27;
+                return newvalue;
+            }
+            public override Currency ToPound()
+            {
+                Dollar newvalue = new Dollar();
+                newvalue.number = this.number * 2;
+                return newvalue;
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}", this.number);
+            }
+        }
+        public class Pounds : Currency
+        {
+            public double number
+            {
+                get
+                {
+                    return Money;
+                }
+                set
+                {
+                    Money = value;
+                }
+            }
+
+            public override Currency ToDollar()
+            {
+                Pounds newvalue = new Pounds();
+                newvalue.number = this.number/2;
+                return newvalue;
+            }
+
+            public override Currency ToEuro()
+            {
+                Pounds newvalue = new Pounds();
+                newvalue.number = this.number/1.8;
+                return newvalue;
+            }
+            public override Currency ToHryvnya()
+            {
+                Pounds newvalue = new Pounds();
+                newvalue.number = this.number * 40;
+                return newvalue;
+            }
+            public override Currency ToPound()
+            {
+                Dollar newvalue = new Dollar();
+                newvalue.number = this.number;
+                return newvalue;
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}", this.number);
+            }
+        }
+        public class Euro : Currency
+        {
+            public double number
+            {
+                get
+                {
+                    return Money;
+                }
+                set
+                {
+                    Money = value;
+                }
+            }
+
+            public override Currency ToDollar()
+            {
+                Euro newvalue = new Euro();
+                newvalue.number = this.number/1.2;
+                return newvalue;
+            }
+
+            public override Currency ToEuro()
+            {
+                Euro newvalue = new Euro();
+                newvalue.number = this.number;
+                return newvalue;
+            }
+            public override Currency ToHryvnya()
+            {
+                Euro newvalue = new Euro();
+                newvalue.number = this.number * 35;
+                return newvalue;
+            }
+            public override Currency ToPound()
+            {
+                Euro newvalue = new Euro();
+                newvalue.number = this.number * 2;
+                return newvalue;
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}", this.number);
+            }
+        }
+        public class Purse : List<Currency>
+        {
+            private int size;
+            public Currency[] array;
+            private int index = 0;
+            public Purse(int sized)
+            {
+                size = sized;
+                array = new Currency[size];
+
+            }
+            public new Boolean Add (Currency x) {
+                array[index] = x;
+                index++;
+                return true;
+            }
+            public int length
+            {
+                get
+                {
+                    return array.Length;
+                }
+            }
+            public override string ToString()
+            {
+                 string vivod = "";
+                for (int i = 0; i < length; i++)
+                {
+                    vivod += string.Format(i + 1 + ". {0} \n", this.array[i]);
+                
+                }
+                return vivod;
+            }
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Purse))
+                {
+                    return false;
+                }
+                else
+                {
+                    return ((Purse)obj).array == this.array;
+                }
+                 
+            }
+            public static bool operator == (Purse one, Purse two)
+            {
+                if ((Object)one == null || (Object)two == null)//проверить на null
+                    return false;
+
+                return one.Equals(two);
+            }
+            public static bool operator != (Purse one, Purse two)
+            {
+                if ((Object)one == null || (Object)two == null)//проверить на null
+                    return true;
+
+                return !one.Equals(two);
+            }
+            public override int GetHashCode()
+            {
+                try
+                {
+                    return Convert.ToInt32(this.array);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return -1;
+                }
+            }
+            public Purse DeepCopy()
+            {
+                Purse obj = new Purse(size);
+                return obj;
+            }
+            class MyInvalidCastException : InvalidCastException
+            {
+                public MyInvalidCastException()
+                    : base() { }
+
+                public MyInvalidCastException(string message)
+                    : base(message) { }
+
+                public MyInvalidCastException(string format, params object[] args)
+                    : base(string.Format(format, args)) { }
+
+                public MyInvalidCastException(string message, Exception innerException)
+                    : base(message, innerException) { }
+
+                public MyInvalidCastException(string format, Exception innerException, params object[] args)
+                    : base(string.Format(format, args), innerException) { }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Вот лаба");
+            Dollar first = new Dollar();
+            first.number = 80.76;
+            Euro second = new Euro();
+            second.number = 36.36;
+            Pounds third = new Pounds();
+            third.number = 88.88;
+            Console.WriteLine("Доллары: " + first.number);
+            Console.WriteLine("Доллары в гривну: " + (first.ToHryvnya()).ToString());
+            Console.WriteLine("В доллары обратно: " + first.number);
+            Console.WriteLine("Евро: " + second.number);
+            Console.WriteLine("Евро в гривну: " + (second.ToHryvnya()).ToString());
+            Console.WriteLine("фунты: " + third.number);
+            Console.WriteLine("Фунты в гривну: " + (third.ToHryvnya()).ToString());
+            Purse Gamanets = new Purse(3);
+            Gamanets.Add(first);
+            Gamanets.Add(second);
+            Gamanets.Add(third);
+            Console.WriteLine("Мои деньги в гаманце:\n" + Gamanets.ToString());
+        }
+    }
+}
+
